31449fdedaf5331e81cfacca6087cfbdb200583c
Agam Gusriyandi <agamgusriyandi@gmail.com>
2016-01-17 00:00:00 +0000

    feat: add web scraper utility

---
+++ scraper.py
@@ -0,0 +1,8 @@
+# Updated: 2016-01-17
+import requests
+from bs4 import BeautifulSoup
+
+def scrape(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    return soup.find_all('a')
