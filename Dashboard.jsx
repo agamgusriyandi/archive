9533471f63a551f78716f702d3c966bcf207e6ea
Agam Gusriyandi <agamgusriyandi@gmail.com>
2016-01-22 00:00:00 +0000

    feat: add dashboard with data fetching

---
+++ Dashboard.jsx
@@ -0,0 +1,10 @@
+# Updated: 2016-01-22
+import React, { useState, useEffect } from 'react';
+
+export default function Dashboard() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('/api/data').then(r => r.json()).then(setData);
+  }, []);
+  return <div className='dashboard'>{data.length} items</div>;
+}
