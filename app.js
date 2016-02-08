6863593eb3a150dd375c76b443badabcd7ee8d4d
Agam Gusriyandi <agamgusriyandi@gmail.com>
2016-02-08 00:00:00 +0000

    feat: setup Express.js server

---
+++ app.js
@@ -0,0 +1,7 @@
+# Updated: 2016-02-08
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ status: 'running' }));
+app.listen(3000, () => console.log('Server started'));
