0d9219ab3dcf1a7e46b8fc53f190d26ca65376a7
Agam Gusriyandi <agamgusriyandi@gmail.com>
2016-02-11 00:00:00 +0000

    feat: add deployment shell script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# Updated: 2016-02-11
+#!/bin/bash
+set -e
+echo 'Pulling latest code...'
+git pull origin main
+echo 'Installing dependencies...'
+npm install
+echo 'Restarting service...'
+pm2 restart app
+echo 'Deploy complete!'
