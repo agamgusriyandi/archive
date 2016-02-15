88e1a6091e99c989ca9c235cc6c82bf76dbad2ac
Agam Gusriyandi <agamgusriyandi@gmail.com>
2016-02-15 00:00:00 +0000

    feat: add user CRUD routes

---
+++ routes.js
@@ -0,0 +1,7 @@
+# Updated: 2016-02-15
+const router = require('express').Router();
+router.get('/users', async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
+module.exports = router;
