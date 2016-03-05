a6ccb7fd0dfd7968ed82ec80a44e0b63c0136ae1
Agam Gusriyandi <agamgusriyandi@gmail.com>
2016-03-05 00:00:00 +0000

    feat(grpc_service.go): update 2016-03

---
+++ grpc_service.go
@@ -0,0 +1,21 @@
+// Updated: 2016-03-05
+package service
+import (
+    "context"
+    "google.golang.org/grpc"
+    "log"
+    "net"
+    pb "myapp/proto"
+)
+type server struct { pb.UnimplementedUserServiceServer }
+func (s *server) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.UserResponse, error) {
+    return &pb.UserResponse{Id: req.Id, Name: "Agam", Email: "agam@mail.com"}, nil
+}
+func StartGRPC(addr string) {
+    lis, err := net.Listen("tcp", addr)
+    if err != nil { log.Fatal(err) }
+    s := grpc.NewServer()
+    pb.RegisterUserServiceServer(s, &server{})
+    log.Printf("gRPC server on %s", addr)
+    s.Serve(lis)
+}
