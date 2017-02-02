63c74d6b85e069c0c86e8e9ad4153c32aa86cb28
Agam Gusriyandi <agamgusriyandi@gmail.com>
2017-02-02 00:00:00 +0000

    db: add seed data for testing

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-# Updated: 2017-01-25
+# Updated: 2017-02-02
 INSERT INTO users (name, email, password) VALUES
 ('Admin', 'admin@example.com', 'hashed_password'),
 ('Test User', 'user@example.com', 'hashed_password');
