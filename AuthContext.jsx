5311c2676685c5d579deb645d96577d1625dc10b
Agam Gusriyandi <agamgusriyandi@gmail.com>
2022-04-01 00:00:00 +0000

    feat: implement React auth context

--- AuthContext.jsx
+++ AuthContext.jsx
@@ -1,4 +1,4 @@
-# Updated: 2022-03-18
+# Updated: 2022-04-01
 import { createContext, useState, useContext } from 'react';
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
