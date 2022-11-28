d5808f0f4cedc761a632d79f448f2365b53b969a
Agam Gusriyandi <agamgusriyandi@gmail.com>
2022-11-28 00:00:00 +0000

    feat: add helper utility functions

--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
-# Updated: 2022-11-20
+# Updated: 2022-11-28
 const formatDate = (date) => new Date(date).toLocaleDateString('id-ID');
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 module.exports = { formatDate, capitalize };
