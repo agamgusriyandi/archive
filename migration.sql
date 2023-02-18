e562d2c9cc2dea996ec72d7a808be771d1096bce
Agam Gusriyandi <agamgusriyandi@gmail.com>
2023-02-18 00:00:00 +0000

    db: add role and index migration

--- migration.sql
+++ migration.sql
@@ -1,4 +1,4 @@
-# Updated: 2023-02-11
+# Updated: 2023-02-18
 ALTER TABLE users ADD COLUMN role ENUM('admin','user') DEFAULT 'user';
 ALTER TABLE users ADD COLUMN is_active BOOLEAN DEFAULT TRUE;
 CREATE INDEX idx_email ON users(email);
