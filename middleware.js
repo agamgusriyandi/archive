fb86e1cfb7f2aa0daf5a8ea3b17252cda6a28b97
Agam Gusriyandi <agamgusriyandi@gmail.com>
2023-04-11 00:00:00 +0000

    feat: add JWT auth middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-# Updated: 2023-02-07
+# Updated: 2023-04-11
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) return res.status(401).json({ error: 'Unauthorized' });
